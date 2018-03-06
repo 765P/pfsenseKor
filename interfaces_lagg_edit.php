6df9aced31366fbb35be4f76e28c7d68f4073b5a
TypeScriptKorea <jeawon139@gmail.com>
2018-03-06 10:44:23 +0900

    Update interfaces_lagg_edit.php
    주석, 한글번역 추가

--- interfaces_lagg_edit.php
+++ interfaces_lagg_edit.php
@@ -19,6 +19,11 @@
  * limitations under the License.
  */
 
+/*
+2018.03.06
+한글화 번역 
+*/
+
 ##|+PRIV
 ##|*IDENT=page-interfaces-lagg-edit
 ##|*NAME=Interfaces: LAGG: Edit
@@ -42,50 +47,37 @@ $protohelp =
 '<ul>' .
 	'<li>' .
 		'<strong>' . $laggprotosuc[0] . '</strong><br />' .
-		gettext('This protocol is intended to do nothing: it disables any ' .
-				'traffic without disabling the lagg interface itself.') .
+		gettext('이 프로토콜은 아무 활동이 없으므로, 레이블 인터페이스 자체를 사용하지 않으면서 트래픽을 사용하지 않도록 설정합니다.') .
 	'</li>' .
 	'<li>' .
 		'<strong>' . $laggprotosuc[1] . '</strong><br />' .
-		gettext('Supports the IEEE 802.3ad Link Aggregation Control Protocol ' .
-				'(LACP) and the Marker Protocol.	LACP will negotiate a set ' .
-				'of aggregable links with the peer in to one or more Link ' .
-				'Aggregated Groups.  Each LAG is composed of ports of the ' .
-				'same speed, set to full-duplex operation.  The traffic will ' .
-				'be balanced across the ports in the LAG with the greatest ' .
-				'total speed, in most cases there will only be one LAG which ' .
-				'contains all ports.	In the event of changes in physical ' .
-				'connectivity, Link Aggregation will quickly converge to a ' .
-				'new configuration.') .
+		gettext('IEEE 802.3ad LACP (Link Aggregation Control Protocol) 및 마커 프로토콜을 지원합니다. ' .
+				'LACP는 하나 이상의 링크 집계 그룹에있는 피어와 집합 가능한 링크 집합을 협상합니다. ' .
+				'각 LAG는 전이중 작동으로 설정된 동일한 속도의 포트로 구성됩니다. ' .
+				'트래픽은 LAG의 포트에서 최대 속도로 균형을 유지하게됩니다. ' .
+				'대부분의 경우 모든 포트를 포함하는 LAG가 하나만 있습니다. ' .
+				'물리적 연결이 변경되는 경우 Link Aggregation은 새로운 구성으로 신속하게 수렴됩니다.') .
 	'</li>' .
 	'<li>' .
 		'<strong>' . $laggprotosuc[2] . '</strong><br />' .
-		gettext('Sends and receives traffic only through the master port.  If ' .
-				'the master port becomes unavailable, the next active port is ' .
-				'used.	The first interface added is the master port; any ' .
-				'interfaces added after that are used as failover devices.') .
+		gettext('마스터 포트를 통해서만 트래픽을 보내고받습니다. ' .
+				'마스터 포트를 사용할 수 없게되면 다음 활성 포트가 사용됩니다. ' .
+				'추가 된 첫 번째 인터페이스는 마스터 포트입니다. 이후에 추가 된 인터페이스는 페일 오버 장치로 사용됩니다.') .
 	'</li>' .
 	'<li>' .
 		'<strong>' . $laggprotosuc[3] . '</strong><br />' .
-		gettext('Supports Cisco EtherChannel.  This is a static setup and ' .
-				'does not negotiate aggregation with the peer or exchange ' .
-				'frames to monitor the link.') .
+		gettext('Cisco EtherChannel을 지원합니다. ' .
+				'이것은 정적 설정이며 링크를 모니터링하기 위해 피어 또는 교환 프레임과 집합을 협상하지 않습니다.') .
 	'</li>' .
 	'<li>' .
 		'<strong>' . $laggprotosuc[4] . '</strong><br />' .
-		gettext('Balances outgoing traffic across the active ports based on ' .
-				'hashed protocol header information and accepts incoming ' .
-				'traffic from any active port.	 This is a static setup and ' .
-				'does not negotiate aggregation with the peer or exchange ' .
-				'frames to monitor the link.  The hash includes the Ethernet ' .
-				'source and destination address, and, if available, the VLAN ' .
-				'tag, and the IP source and destination address.') .
+		gettext('해시 된 프로토콜 헤더 정보를 기반으로 활성 포트에서 나가는 트래픽의 균형을 맞추고 활성 포트에서 들어오는 트래픽을 허용합니다. ' .
+				'이것은 정적 설정이며 링크를 모니터링하기 위해 피어 또는 교환 프레임과 집합을 협상하지 않습니다. ' .
+				'해시에는 이더넷 원본 및 대상 주소와 사용 가능한 경우 VLAN 태그와 IP 원본 및 대상 주소가 포함됩니다.') .
 	'</li>' .
 	'<li>' .
 		'<strong>' . $laggprotosuc[5] . '</strong><br />' .
-		gettext('Distributes outgoing traffic using a round-robin scheduler ' .
-				'through all active ports and accepts incoming traffic from ' .
-				'any active port.') .
+		gettext('모든 활성 포트를 통해 라운드 로빈 스케줄러를 사용하여 나가는 트래픽을 분배하고 활성 포트에서 들어오는 트래픽을 수용합니다.') .
 	'</li>' .
 '</ul>';
 
@@ -130,22 +122,22 @@ if ($_POST['save']) {
 
 	/* input validation */
 	$reqdfields = explode(" ", "members proto");
-	$reqdfieldsn = array(gettext("Member interfaces"), gettext("Lagg protocol"));
+	$reqdfieldsn = array(gettext("멤버 인터페이스"), gettext("Lagg 프로토콜"));
 
 	do_input_validation($_POST, $reqdfields, $reqdfieldsn, $input_errors);
 
 	if (is_array($_POST['members'])) {
 		foreach ($_POST['members'] as $member) {
 			if (!does_interface_exist($member)) {
-				$input_errors[] = sprintf(gettext("Interface supplied as member (%s) is invalid"), $member);
+				$input_errors[] = sprintf(gettext("멤버로 제공된 인터페이스가 잘못되었습니다(%s)."), $member);
 			}
 		}
 	} else if (!does_interface_exist($_POST['members'])) {
-		$input_errors[] = gettext("Interface supplied as member is invalid");
+		$input_errors[] = gettext("멤버로 제공된 인터페이스가 잘못되었습니다.");
 	}
 
 	if (!in_array($_POST['proto'], $laggprotos)) {
-		$input_errors[] = gettext("Protocol supplied is invalid");
+		$input_errors[] = gettext("제공된 프로토콜이 잘못되었습니다.");
 	}
 
 	if (!$input_errors) {
@@ -160,7 +152,7 @@ if ($_POST['save']) {
 
 		$lagg['laggif'] = interface_lagg_configure($lagg);
 		if ($lagg['laggif'] == "" || !stristr($lagg['laggif'], "lagg")) {
-			$input_errors[] = gettext("Error occurred creating interface, please retry.");
+			$input_errors[] = gettext("인터페이스를 생성하는 동안 오류가 발생했습니다. 다시 시도하십시오.");
 		} else {
 			if (isset($id) && $a_laggs[$id]) {
 				$a_laggs[$id] = $lagg;
@@ -214,7 +206,7 @@ function build_member_list() {
 	return($memberlist);
 }
 
-$pgtitle = array(gettext("Interfaces"), gettext("LAGGs"), gettext("Edit"));
+$pgtitle = array(gettext("Interfaces"), gettext("LAGGs"), gettext("편집"));
 $pglinks = array("", "interfaces_lagg.php", "@self");
 $shortcut_section = "interfaces";
 include("head.inc");
