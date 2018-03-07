aa738a25f8ff13109dda3e150c86d5d3aea476da
TypeScriptKorea <jeawon139@gmail.com>
2018-03-07 15:39:39 +0900

    Update services_wol.php
    주석, 한글번역

--- services_wol.php
+++ services_wol.php
@@ -23,6 +23,11 @@
  * limitations under the License.
  */
 
+/*
+2018.03.07
+한글화 번역 
+*/
+
 ##|+PRIV
 ##|*IDENT=page-services-wakeonlan
 ##|*NAME=Services: Wake-on-LAN
@@ -50,10 +55,10 @@ if ($_REQUEST['wakeall'] != "") {
 		$bcip = gen_subnet_max($ipaddr, get_interface_subnet($if));
 		/* Execute wol command and check return code. */
 		if (!mwexec("/usr/local/bin/wol -i {$bcip} {$mac}")) {
-			$savemsg .= sprintf(gettext('Sent magic packet to %1$s (%2$s).'), $mac, $description) . "<br />";
+			$savemsg .= sprintf(gettext('매직 패킷을 %1$s(%2$s)(으)로 보냈습니다.'), $mac, $description) . "<br />";
 			$class = 'success';
 		} else {
-			$savemsg .= sprintf(gettext('Please check the %1$ssystem log%2$s, the wol command for %3$s (%4$s) did not complete successfully.'), '<a href="/status_logs.php">', '</a>', $description, $mac) . "<br />";
+			$savemsg .= sprintf(gettext('%1$s 시스템 로그 %2$s를 확인하십시오. %3$s (%4$s)의 wol 명령이 성공적으로 완료되지 않았습니다.'), '<a href="/status_logs.php">', '</a>', $description, $mac) . "<br />";
 			$class = 'warning';
 		}
 	}
@@ -70,11 +75,11 @@ if ($_POST['Submit'] || $_POST['mac']) {
 
 	/* input validation */
 	if (!$mac || !is_macaddr($mac)) {
-		$input_errors[] = gettext("A valid MAC address must be specified.");
+		$input_errors[] = gettext("유효한 MAC 주소를 지정해야합니다.");
 	}
 
 	if (!$if) {
-		$input_errors[] = gettext("A valid interface must be specified.");
+		$input_errors[] = gettext("유효한 인터페이스를 지정해야합니다.");
 	}
 
 	if (!$input_errors) {
@@ -86,10 +91,10 @@ if ($_POST['Submit'] || $_POST['mac']) {
 			$bcip = gen_subnet_max($ipaddr, get_interface_subnet($if));
 			/* Execute wol command and check return code. */
 			if (!mwexec("/usr/local/bin/wol -i {$bcip} " . escapeshellarg($mac))) {
-				$savemsg .= sprintf(gettext("Sent magic packet to %s."), $mac);
+				$savemsg .= sprintf(gettext("%s에 매직 패킷을 보냈습니다."), $mac);
 				$class = 'success';
 			} else {
-				$savemsg .= sprintf(gettext('Please check the %1$ssystem log%2$s, the wol command for %3$s did not complete successfully.'), '<a href="/status_logs.php">', '</a>', $mac) . "<br />";
+				$savemsg .= sprintf(gettext('%1$s 시스템 로그 %2$s를 확인하십시오. %3$s의 wol 명령이 성공적으로 완료되지 않았습니다.'), '<a href="/status_logs.php">', '</a>', $mac) . "<br />";
 				$class = 'warning';
 			}
 		}
@@ -99,19 +104,19 @@ if ($_POST['Submit'] || $_POST['mac']) {
 if ($_POST['act'] == "del") {
 	if ($a_wol[$_POST['id']]) {
 		unset($a_wol[$_POST['id']]);
-		write_config(gettext("Deleted a device from WOL configuration."));
+		write_config(gettext("WOL 구성에서 장치를 삭제했습니다."));
 		header("Location: services_wol.php");
 		exit;
 	}
 }
 
-$pgtitle = array(gettext("Services"), gettext("Wake-on-LAN"));
+$pgtitle = array(gettext("서비스"), gettext("Wake-on-LAN"));
 include("head.inc");
 ?>
 <div class="infoblock blockopen">
 <?php
-print_info_box(gettext('This service can be used to wake up (power on) computers by sending special "Magic Packets".') . '<br />' .
-			   gettext('The NIC in the computer that is to be woken up must support Wake-on-LAN and must be properly configured (WOL cable, BIOS settings).'),
+print_info_box(gettext('이 서비스는 특별한 "매직 패킷"을 보내 컴퓨터를 깨우거나 (켜기 위해) 사용할 수 있습니다.') . '<br />' .
+			   gettext('깨우는 컴퓨터의 NIC는 WOL(Wake-on-LAN)을 지원해야하며 제대로 구성되어야합니다 (WOL 케이블, BIOS 설정).'),
 			   'info', false);
 
 ?>
@@ -158,17 +163,17 @@ print $form;
 
 <div class="panel panel-default">
 	<div class="panel-heading">
-		<h2 class="panel-title"><?=gettext("Wake-on-LAN Devices");?></h2>
+		<h2 class="panel-title"><?=gettext("Wake-on-LAN 디바이스");?></h2>
 	</div>
 
 	<div class="panel-body">
-		<p><?=gettext("Click the MAC address to wake up an individual device.")?></p>
+		<p><?=gettext("개별 장치를 깨우려면 MAC 주소를 클릭하십시오.")?></p>
 		<div class="table-responsive">
 			<table class="table table-striped table-hover table-rowdblclickedit">
 				<thead>
 					<tr>
-						<th><?=gettext("Interface")?></th>
-						<th><?=gettext("MAC address")?></th>
+						<th><?=gettext("인터페이스")?></th>
+						<th><?=gettext("MAC 주소")?></th>
 						<th><?=gettext("Description")?></th>
 						<th><?=gettext("Actions")?></th>
 					</tr>
@@ -186,9 +191,9 @@ print $form;
 								<?=htmlspecialchars($wolent['descr']);?>
 							</td>
 							<td>
-								<a class="fa fa-pencil"	title="<?=gettext('Edit Device')?>"	href="services_wol_edit.php?id=<?=$i?>"></a>
-								<a class="fa fa-trash"	title="<?=gettext('Delete Device')?>" href="services_wol.php?act=del&amp;id=<?=$i?>" usepost></a>
-								<a class="fa fa-power-off" title="<?=gettext('Wake Device')?>" href="?mac=<?=$wolent['mac'];?>&amp;if=<?=$wolent['interface'];?>" usepost></a>
+								<a class="fa fa-pencil"	title="<?=gettext('디바이스 편집')?>"	href="services_wol_edit.php?id=<?=$i?>"></a>
+								<a class="fa fa-trash"	title="<?=gettext('디바이스 삭제')?>" href="services_wol.php?act=del&amp;id=<?=$i?>" usepost></a>
+								<a class="fa fa-power-off" title="<?=gettext('디바이스 깨우기')?>" href="?mac=<?=$wolent['mac'];?>&amp;if=<?=$wolent['interface'];?>" usepost></a>
 							</td>
 						</tr>
 					<?php endforeach?>
@@ -199,12 +204,12 @@ print $form;
 	<div class="panel-footer">
 		<a class="btn btn-success" href="services_wol_edit.php">
 			<i class="fa fa-plus icon-embed-btn"></i>
-			<?=gettext("Add");?>
+			<?=gettext("추가");?>
 		</a>
 
 		<a href="services_wol.php?wakeall=true" role="button" class="btn btn-primary">
 			<i class="fa fa-power-off icon-embed-btn"></i>
-			<?=gettext("Wake All Devices")?>
+			<?=gettext("모든 디바이스 ")?>
 		</a>
 	</div>
 </div>
