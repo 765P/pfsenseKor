e0628c015b0428ce284066778ed771cd7396852b
TypeScriptKorea <jeawon139@gmail.com>
2018-03-15 10:44:11 +0900

    Update services_unbound_acls.php
    주석, 한글번역 추가

--- services_unbound_acls.php
+++ services_unbound_acls.php
@@ -20,6 +20,11 @@
  * limitations under the License.
  */
 
+/*
+2018.03.15
+한글화 번역 시작
+*/
+
 ##|+PRIV
 ##|*IDENT=page-services-dnsresolver-acls
 ##|*NAME=Services: DNS Resolver: Access Lists
@@ -57,7 +62,7 @@ if ($_POST['act'] == "del") {
 	}
 
 	unset($a_acls[$id]);
-	write_config(gettext("Access list deleted from DNS Resolver."));
+	write_config(gettext("액세스 목록이 DNS 확인자에서 삭제되었습니다."));
 	mark_subsystem_dirty('unbound');
 }
 
@@ -101,21 +106,21 @@ if ($_POST['save']) {
 			$networkacl[$x]['mask'] = $pconfig["mask{$x}"];
 			$networkacl[$x]['description'] = $pconfig["description{$x}"];
 			if (!is_ipaddr($networkacl[$x]['acl_network'])) {
-				$input_errors[] = gettext("A valid IP address must be entered for each row under Networks.");
+				$input_errors[] = gettext("네트워크 아래의 각 행에 유효한 IP 주소를 입력해야합니다.");
 			}
 
 			if (is_ipaddr($networkacl[$x]['acl_network'])) {
 				if (!is_subnet($networkacl[$x]['acl_network']."/".$networkacl[$x]['mask'])) {
-					$input_errors[] = gettext("A valid IPv4 netmask must be entered for each IPv4 row under Networks.");
+					$input_errors[] = gettext("네트워크 아래의 각 IPv4 행에 유효한 IPv4 넷 마스크를 입력해야합니다.");
 				}
 			} else if (function_exists("is_ipaddrv6")) {
 				if (!is_ipaddrv6($networkacl[$x]['acl_network'])) {
-					$input_errors[] = gettext("A valid IPv6 address must be entered for {$networkacl[$x]['acl_network']}.");
+					$input_errors[] = gettext("{$networkacl[$x]['acl_network']}에 유효한 IPv6 주소를 입력해야합니다.");
 				} else if (!is_subnetv6($networkacl[$x]['acl_network']."/".$networkacl[$x]['mask'])) {
-					$input_errors[] = gettext("A valid IPv6 netmask must be entered for each IPv6 row under Networks.");
+					$input_errors[] = gettext("네트워크 아래의 각 IPv6 행에 유효한 IPv6 넷 마스크를 입력해야합니다.");
 				}
 			} else {
-				$input_errors[] = gettext("A valid IP address must be entered for each row under Networks.");
+				$input_errors[] = gettext("네트워크 아래의 각 행에 유효한 IP 주소를 입력해야합니다.");
 			}
 		} else if (isset($networkacl[$x])) {
 			unset($networkacl[$x]);
@@ -123,7 +128,7 @@ if ($_POST['save']) {
 	}
 
 	if (!$input_errors) {
-		if (strtolower($pconfig['save']) == gettext("save")) {
+		if (strtolower($pconfig['save']) == gettext("저장")) {
 			$acl_entry = array();
 			$acl_entry['aclid'] = $pconfig['aclid'];
 			$acl_entry['aclname'] = $pconfig['aclname'];
@@ -143,7 +148,7 @@ if ($_POST['save']) {
 			}
 
 			mark_subsystem_dirty("unbound");
-			write_config(gettext("Access list configured for DNS Resolver."));
+			write_config(gettext("DNS 확인자에 대해 구성된 액세스 목록"));
 
 			pfSenseHeader("/services_unbound_acls.php");
 			exit;
@@ -152,14 +157,14 @@ if ($_POST['save']) {
 }
 
 $actionHelp =
-					sprintf(gettext('%1$sDeny:%2$s Stops queries from hosts within the netblock defined below.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
-					sprintf(gettext('%1$sRefuse:%2$s Stops queries from hosts within the netblock defined below, but sends a DNS rcode REFUSED error message back to the client.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
-					sprintf(gettext('%1$sAllow:%2$s Allow queries from hosts within the netblock defined below.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
-					sprintf(gettext('%1$sAllow Snoop:%2$s Allow recursive and nonrecursive access from hosts within the netblock defined below. Used for cache snooping and ideally should only be configured for the administrative host.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
-					sprintf(gettext('%1$sDeny Nonlocal:%2$s Allow only authoritative local-data queries from hosts within the netblock defined below. Messages that are disallowed are dropped.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
-					sprintf(gettext('%1$sRefuse Nonlocal:%2$s Allow only authoritative local-data queries from hosts within the netblock defined below. Sends a DNS rcode REFUSED error message back to the client for messages that are disallowed.'), '<span class="text-success"><strong>', '</strong></span>');
+					sprintf(gettext('%1$sDeny:%2$s 아래 정의 된 넷 블록 내의 호스트에서 쿼리를 중지합니다.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
+					sprintf(gettext('%1$sRefuse:%2$s 아래에 정의 된 넷 블록 내의 호스트에서 쿼리를 중지하지만 DNS rcode REFUSED 오류 메시지를 클라이언트에 보냅니다.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
+					sprintf(gettext('%1$sAllow:%2$s 아래에 정의 된 넷 블록 내의 호스트로부터의 쿼리를 허용합니다.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
+					sprintf(gettext('%1$sAllow Snoop:%2$s  아래에 정의 된 넷 블록 내의 호스트로부터 재귀 및 비 재귀 액세스를 허용합니다. 캐시 스누핑에 사용되며 이상적으로는 관리 호스트에 대해서만 구성되어야합니다.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
+					sprintf(gettext('%1$sDeny Nonlocal:%2$s 아래에 정의 된 넷 블록 내의 호스트로부터 신뢰할 수있는 로컬 데이터 쿼리 만 허용합니다. 허용되지 않는 메시지는 삭제됩니다.%3$s'), '<span class="text-success"><strong>', '</strong></span>', '<br />') .
+					sprintf(gettext('%1$sRefuse Nonlocal:%2$s 아래에 정의 된 넷 블록 내의 호스트로부터 신뢰할 수있는 로컬 데이터 쿼리 만 허용합니다. DNS rcode REFUSED 오류 메시지를 클라이언트에 보내서 허용되지 않는 메시지를 다시 보냅니다.'), '<span class="text-success"><strong>', '</strong></span>');
 
-$pgtitle = array(gettext("Services"), gettext("DNS Resolver"), gettext("Access Lists"));
+$pgtitle = array(gettext("Services"), gettext("DNS 확인자"), gettext("액세스리스트"));
 $pglinks = array("", "services_unbound.php", "@self");
 
 if ($act == "new" || $act == "edit") {
@@ -177,13 +182,13 @@ if ($_POST['apply']) {
 }
 
 if (is_subsystem_dirty('unbound')) {
-	print_apply_box(gettext("The DNS resolver configuration has been changed.") . "<br />" . gettext("The changes must be applied for them to take effect."));
+	print_apply_box(gettext("DNS확인자 구성이 변경되었습니다.") . "<br />" . gettext("변경사항을 저장하시면 적용됩니다."));
 }
 
 $tab_array = array();
-$tab_array[] = array(gettext("General Settings"), false, "/services_unbound.php");
-$tab_array[] = array(gettext("Advanced Settings"), false, "services_unbound_advanced.php");
-$tab_array[] = array(gettext("Access Lists"), true, "/services_unbound_acls.php");
+$tab_array[] = array(gettext("일반설정"), false, "/services_unbound.php");
+$tab_array[] = array(gettext("고급설정"), false, "services_unbound_advanced.php");
+$tab_array[] = array(gettext("액세스 목록"), true, "/services_unbound_acls.php");
 display_top_tabs($tab_array, true);
 
 if ($act == "new" || $act == "edit") {
@@ -217,7 +222,7 @@ if ($act == "new" || $act == "edit") {
 		'aclaction',
 		'*Action',
 		strtolower($pconfig['aclaction']),
-		array('allow' => gettext('Allow'), 'deny' => gettext('Deny'), 'refuse' => gettext('Refuse'), 'allow snoop' => gettext('Allow Snoop'), 'deny nonlocal' => gettext('Deny Nonlocal'), 'refuse nonlocal' => gettext('Refuse Nonlocal'))
+		array('allow' => gettext('허가'), 'deny' => gettext('Deny'), 'refuse' => gettext('거부'), 'allow snoop' => gettext('Allow Snoop'), 'deny nonlocal' => gettext('Deny Nonlocal'), 'refuse nonlocal' => gettext('Refuse Nonlocal'))
 	))->setHelp($actionHelp);
 
 	$section->addInput(new Form_Input(
@@ -276,15 +281,15 @@ if ($act == "new" || $act == "edit") {
 	// NOT 'edit' or 'add'
 ?>
 <div class="panel panel-default">
-	<div class="panel-heading"><h2 class="panel-title"><?=gettext('Access Lists to Control Access to the DNS Resolver')?></h2></div>
+	<div class="panel-heading"><h2 class="panel-title"><?=gettext('DNS확인자에 대한 액세스 제어')?></h2></div>
 	<div class="panel-body">
 		<div class="table-responsive">
 			<table class="table table-striped table-hover table-condensed sortable-theme-bootstrap" data-sortable>
 				<thead>
 					<tr>
-						<th><?=gettext("Access List Name")?></th>
+						<th><?=gettext("액세스 목록 이름")?></th>
 						<th><?=gettext("Action")?></th>
-						<th><?=gettext("Description")?></th>
+						<th><?=gettext("설명")?></th>
 						<th><?=gettext("Actions")?></th>
 					</tr>
 				</thead>
@@ -304,8 +309,8 @@ if ($act == "new" || $act == "edit") {
 							<?=htmlspecialchars($acl['description'])?>
 						</td>
 						<td>
-							<a class="fa fa-pencil"	title="<?=gettext('Edit ACL')?>" href="services_unbound_acls.php?act=edit&amp;id=<?=$i?>"></a>
-							<a class="fa fa-trash"	title="<?=gettext('Delete ACL')?>" href="services_unbound_acls.php?act=del&amp;id=<?=$i?>" usepost></a>
+							<a class="fa fa-pencil"	title="<?=gettext('ACL 편집')?>" href="services_unbound_acls.php?act=edit&amp;id=<?=$i?>"></a>
+							<a class="fa fa-trash"	title="<?=gettext('ACL 삭제')?>" href="services_unbound_acls.php?act=del&amp;id=<?=$i?>" usepost></a>
 						</td>
 					</tr>
 <?php
