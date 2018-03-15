2d25ed09d27b861c1454bdbfd62f17c0a34b3ec4
TypeScriptKorea <jeawon139@gmail.com>
2018-03-15 09:26:22 +0900

    Update interfaces_vlan.php
    주석, 한글번역 시작

--- interfaces_vlan.php
+++ interfaces_vlan.php
@@ -23,6 +23,11 @@
  * limitations under the License.
  */
 
+/*
+2018.03.15
+한글화 번역 시작
+*/
+
 ##|+PRIV
 ##|*IDENT=page-interfaces-vlan
 ##|*NAME=Interfaces: VLAN
@@ -43,12 +48,12 @@ $a_vlans = &$config['vlans']['vlan'] ;
 
 if ($_POST['act'] == "del") {
 	if (!isset($_POST['id'])) {
-		$input_errors[] = gettext("Wrong parameters supplied");
+		$input_errors[] = gettext("잘못된 매개 변수가 제공되었습니다.");
 	} else if (empty($a_vlans[$_POST['id']])) {
-		$input_errors[] = gettext("Wrong index supplied");
+		$input_errors[] = gettext("잘못된 색인이 제공되었습니다.");
 	/* check if still in use */
 	} else if (vlan_inuse($a_vlans[$_POST['id']])) {
-		$input_errors[] = gettext("This VLAN cannot be deleted because it is still being used as an interface.");
+		$input_errors[] = gettext("이 VLAN은 여전히 인터페이스로 사용 중이므로 삭제할 수 없습니다.");
 	} else {
 		if (does_interface_exist($a_vlans[$_POST['id']]['vlanif'])) {
 			pfSense_interface_destroy($a_vlans[$_POST['id']]['vlanif']);
@@ -63,16 +68,16 @@ if ($_POST['act'] == "del") {
 }
 
 
-$pgtitle = array(gettext("Interfaces"), gettext("VLANs"));
+$pgtitle = array(gettext("인터페이스"), gettext("VLANs"));
 $shortcut_section = "interfaces";
 include('head.inc');
 
 if ($input_errors) print_input_errors($input_errors);
 
 $tab_array = array();
-$tab_array[] = array(gettext("Interface Assignments"), false, "interfaces_assign.php");
-$tab_array[] = array(gettext("Interface Groups"), false, "interfaces_groups.php");
-$tab_array[] = array(gettext("Wireless"), false, "interfaces_wireless.php");
+$tab_array[] = array(gettext("인터페이스 할당"), false, "interfaces_assign.php");
+$tab_array[] = array(gettext("인터페이스 그룹"), false, "interfaces_groups.php");
+$tab_array[] = array(gettext("무선통신"), false, "interfaces_wireless.php");
 $tab_array[] = array(gettext("VLANs"), true, "interfaces_vlan.php");
 $tab_array[] = array(gettext("QinQs"), false, "interfaces_qinq.php");
 $tab_array[] = array(gettext("PPPs"), false, "interfaces_ppps.php");
@@ -88,15 +93,15 @@ display_top_tabs($tab_array);
 	<input id="id" type="hidden" name="id" value=""/>
 
 	<div class="panel panel-default">
-		<div class="panel-heading"><h2 class="panel-title"><?=gettext('VLAN Interfaces')?></h2></div>
+		<div class="panel-heading"><h2 class="panel-title"><?=gettext('VLAN 인터페이스')?></h2></div>
 		<div class="panel-body">
 			<div class="table-responsive">
 				<table class="table table-striped table-hover table-condensed table-rowdblclickedit">
 					<thead>
 						<tr>
-							<th><?=gettext('Interface');?></th>
-							<th><?=gettext('VLAN tag');?></th>
-							<th><?=gettext('Priority');?></th>
+							<th><?=gettext('인터페이스');?></th>
+							<th><?=gettext('VLAN 태그');?></th>
+							<th><?=gettext('우선 순위');?></th>
 							<th><?=gettext('Description');?></th>
 							<th><?=gettext('Actions');?></th>
 						</tr>
@@ -104,7 +109,7 @@ display_top_tabs($tab_array);
 					<tbody>
 <?php
 	$i = 0;
-	$gettext_array = array('edit'=>gettext('Edit VLAN'),'del'=>gettext('Delete VLAN'));
+	$gettext_array = array('edit'=>gettext('VLAN 편집'),'del'=>gettext('VLAN 제거'));
 	$ifaces = convert_real_interface_to_friendly_interface_name_fast(array());
 	foreach ($a_vlans as $vlan) {
 ?>
@@ -137,21 +142,20 @@ display_top_tabs($tab_array);
 	<nav class="action-buttons">
 		<a class="btn btn-success btn-sm" role="button" href="interfaces_vlan_edit.php">
 			<i class="fa fa-plus icon-embed-btn"></i>
-			<?=gettext('Add'); ?>
+			<?=gettext('추가'); ?>
 		</a>
 	</nav>
 
 </form>
 
 <div class="infoblock">
-	<?php print_info_box(sprintf(gettext('Not all drivers/NICs support 802.1Q '.
-		'VLAN tagging properly. %1$sOn cards that do not explicitly support it, VLAN '.
-		'tagging will still work, but the reduced MTU may cause problems.%1$sSee the '.
-		'%2$s handbook for information on supported cards.'), '<br />', $g['product_name']), 'info', false); ?>
+	<?php print_info_box(sprintf(gettext('모든 드라이버/NIC가 802.1Q VLAN 태깅을 제대로 지원하는 것은 아닙니다.  '.
+		'%1$s 카드를 명시 적으로 지원하지 않으면 VLAN 태깅은 계속 작동하지만 감소 된 MTU로 인해 문제가 발생할 수 있습니다. '.
+		'%1$s 지원되는 카드에 대한 정보는 %2$s 핸드북을 참조하십시오.'), '<br />', $g['product_name']), 'info', false); ?>
 </div>
 
 <?php
-	$delmsg = gettext("Are you sure you want to delete this VLAN?");
+	$delmsg = gettext("VLAN을 정말로 삭제 하시겠습니까?");
 ?>
 
 <script type="text/javascript">
